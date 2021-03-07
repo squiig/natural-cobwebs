16fb9c04ccd9a807c1db87a07110be566274b6cb
Stan <8138029+squiig@users.noreply.github.com>
2021-03-07 19:50:16 +0100

    Added cobweb spawning tasks active for loaded chunks

--- src/main/java/com/cerrealic/naturalcobwebs/GeneralEventListener.java
+++ src/main/java/com/cerrealic/naturalcobwebs/GeneralEventListener.java
@@ -1,14 +1,39 @@
 package com.cerrealic.naturalcobwebs;
 
 import com.cerrealic.naturalcobwebs.events.DebugToggleEvent;
+import com.cerrealic.naturalcobwebs.tasks.CobwebSpawnerTask;
+import org.bukkit.Chunk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GeneralEventListener implements Listener {
 	private final NaturalCobwebsConfig config;
+	private Map<Chunk, CobwebSpawnerTask> spawnerTasks;
 
 	public GeneralEventListener(NaturalCobwebsPlugin plugin) {
 		this.config = plugin.getNaturalCobwebsConfig();
+		this.spawnerTasks = new HashMap<>();
+	}
+
+	@EventHandler
+	public void onChunkLoad(ChunkLoadEvent event) {
+		Chunk chunk = event.getChunk();
+		CobwebSpawnerTask spawnerTask = new CobwebSpawnerTask(chunk, 0, 200, 1);
+		spawnerTasks.put(chunk, spawnerTask);
+		spawnerTask.start();
+	}
+
+	@EventHandler
+	public void onChunkUnload(ChunkUnloadEvent event) {
+		if (spawnerTasks.containsKey(event.getChunk())) {
+			spawnerTasks.get(event.getChunk()).stop();
+			spawnerTasks.remove(event.getChunk());
+		}
 	}
 
 	@EventHandler
---
+++ src/main/java/com/cerrealic/naturalcobwebs/tasks/CobwebSpawnerTask.java
@@ -0,0 +1,79 @@
+package com.cerrealic.naturalcobwebs.tasks;
+
+import com.cerrealic.naturalcobwebs.NaturalCobwebsPlugin;
+import org.bukkit.Chunk;
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.Material;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.Random;
+
+public class CobwebSpawnerTask {
+	private BukkitTask runningTask;
+	private final int delay;
+	private final int period;
+	private final Chunk chunk;
+	private final Random random;
+	private final float spawnChance;
+
+	public CobwebSpawnerTask(Chunk chunk, int delay, int period, float spawnChance) {
+		this.chunk = chunk;
+		this.delay = delay;
+		this.period = period;
+		this.random = new Random();
+		this.spawnChance = spawnChance;
+	}
+
+	public CobwebSpawnerTask start() {
+		runningTask = new BukkitRunnable() {
+			@Override
+			public void run() {
+				attemptSpawn();
+			}
+		}.runTaskTimer(NaturalCobwebsPlugin.getInstance(), delay, period);
+		return this;
+	}
+
+	public void stop() {
+		if (isRunning()) {
+			runningTask.cancel();
+		}
+	}
+
+	public BukkitTask getRunningTask() {
+		return runningTask;
+	}
+
+	public boolean isRunning() {
+		return runningTask != null && !runningTask.isCancelled();
+	}
+
+	private void attemptSpawn() {
+		ChunkSnapshot cs = chunk.getChunkSnapshot();
+		for (int x = 0; x < 16; x++) {
+			for (int z = 0; z < 16; z++) {
+				int columnHeight = cs.getHighestBlockYAt(x, z);
+				for (int y = 0; y < columnHeight; y++) {
+					Material mat = cs.getBlockType(x, y, z);
+					if (isValidSpawnCoord(cs, x, y, z) && random.nextFloat() < spawnChance) {
+						chunk.getBlock(x, y, z).setType(Material.COBWEB);
+					}
+				}
+			}
+		}
+	}
+
+	private boolean isValidSpawnCoord(ChunkSnapshot chunkSnapshot, int x, int y, int z) {
+		Material mat = chunkSnapshot.getBlockType(x, y, z);
+		Material matBelow = chunkSnapshot.getBlockType(x, y - 1, z);
+		Material matAbove = chunkSnapshot.getBlockType(x, y + 1, z);
+		Material matNorth = chunkSnapshot.getBlockType(x, y, z + 1);
+		Material matSouth = chunkSnapshot.getBlockType(x, y, z - 1);
+		Material matEast = chunkSnapshot.getBlockType(x + 1, y, z);
+		Material matWest = chunkSnapshot.getBlockType(x - 1, y, z);
+
+		return mat.isAir() && matAbove.isSolid() &&
+				(matNorth.isSolid() || matSouth.isSolid() || matEast.isSolid() || matWest.isSolid());
+	}
+}
